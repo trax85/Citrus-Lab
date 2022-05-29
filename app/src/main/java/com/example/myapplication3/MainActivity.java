9195d36e6d08eb3106d1400236ef4dda9f3cf68b
Tejas Udupa <tejasudupa1285@gmail.com>
2022-05-29 11:19:03 +0530

    activity_main:Switch to bottomsheet layout
    - remove dialogue and its animation assets.
    - Switch to bottomsheet layout implimentaion and open it on
    touchevent on the navdrawer icon.
    - encapsulate viewpager in Listview which is used for dimming
    rest of page when the bottomsheet is open.
    - set icon and text colour in bottompage on select event
    - Update icon set.
    
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -1,31 +1,34 @@
 package com.example.myapplication3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.topjohnwu.superuser.Shell;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
+import android.view.MotionEvent;
+
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.tabs.TabLayout;
-
 public class MainActivity extends AppCompatActivity {
-
-    TabLayout tabLayout;
-    ImageButton bottomsheet;
+    private ImageButton showSheet;
+    private LinearLayout linearLayout;
+    private BottomSheetBehavior bottomSheetBehavior;
+    LinearLayout bottomSheetLayout;
+    private boolean bottomSheetState = true;
     ViewPager2 pa;
+    private static final String TAG = "HomeActivity";
 
     static {
         // Set settings before the main shell can be created
@@ -41,37 +44,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linearLayout = findViewById(R.id.nested_scroll);
+        bottomSheetLayout = findViewById(R.id.bottom_sheet);
         pa = findViewById(R.id.viewPager);
         //Disable swipe left and right
         pa.setUserInputEnabled(false);
+        //Fragment manager setup
         FragmentManager fm = getSupportFragmentManager();
         VPAdaptor sa = new VPAdaptor(fm, getLifecycle());
         pa.setAdapter(sa);
-        bottomsheet = findViewById(R.id.menu);
-        bottomsheet.setOnClickListener(new View.OnClickListener() {
+        //Bottomsheet setup
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
+        bottomSheetBehavior.setPeekHeight(180);
+        //bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
-            public void onClick(View v) {
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetState = true;
+                } else if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetState = false;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                if ( slideOffset>=0 && slideOffset<=1 ) {
+                    linearLayout.setAlpha( 1f - (slideOffset * 0.5f) );
+                }
+            }
+        });
+        showSheet = findViewById(R.id.close_menu);
+        showSheet.setOnClickListener(v -> {
+            if(bottomSheetState == true) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
                 showDialog();
+            }else{
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
             }
         });
-        /*tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                pa.setCurrentItem(tab.getPosition());
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });*/
-        pa.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                //tabLayout.selectTab(tabLayout.getTabAt(position));
-            }
-        });
+        // if input received anywhere on the screen other than bottomsheet
         /*Shell.getShell(shell -> {
             // The main shell is now constructed and cached
             // Exit splash screen and enter main activity
@@ -81,31 +93,49 @@ public class MainActivity extends AppCompatActivity {
         });
         Shell.cmd("su");*/
     }
+    //This method collapses opened bottomsheet when touched outside the bottomsheet
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event){
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (bottomSheetBehavior.getState()==BottomSheetBehavior.STATE_EXPANDED) {
+
+                Rect outRect = new Rect();
+                bottomSheetLayout.getGlobalVisibleRect(outRect);
+
+                if(!outRect.contains((int)event.getRawX(), (int)event.getRawY()))
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        }
+
+        return super.dispatchTouchEvent(event);
+    }
+
     private void showDialog() {
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.bottomsheetlayout);
+        final String GREEN = "#00E676";
+        final String WHITE = "#FFFFFFFF";
+        LinearLayout homelayout = findViewById(R.id.layoutHome);
+        LinearLayout displaylayout = findViewById(R.id.layoutDisplay);
+        TextView textViewDash = findViewById(R.id.textViewDash);
+        TextView textViewDisp = findViewById(R.id.textViewDisp);
+        ImageView imageViewDash = findViewById(R.id.imageViewDash);
+        ImageView imageViewDisp = findViewById(R.id.imageViewDisp);
 
-        LinearLayout homelayout = dialog.findViewById(R.id.layoutHome);
-        LinearLayout displaylayout = dialog.findViewById(R.id.layoutDisplay);
-        LinearLayout blanklayout = dialog.findViewById(R.id.layoutEdit3);
-        ImageButton imageButton = dialog.findViewById(R.id.close_menu);
-
-        imageButton.setOnClickListener(v -> dialog.dismiss());
         homelayout.setOnClickListener(v -> {
             pa.setCurrentItem(0);
-            homelayout.setBackgroundColor(Color.parseColor("#00E676"));
-            dialog.dismiss();
+            textViewDash.setTextColor(Color.parseColor(GREEN));
+            textViewDisp.setTextColor(Color.parseColor(WHITE));
+            imageViewDash.setColorFilter(Color.parseColor(GREEN));
+            imageViewDisp.setColorFilter(Color.parseColor(WHITE));
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         });
         displaylayout.setOnClickListener(v -> {
             pa.setCurrentItem(1);
-            displaylayout.setBackgroundColor(Color.parseColor("#00E676"));
-            dialog.dismiss();
+            textViewDash.setTextColor(Color.parseColor(WHITE));
+            textViewDisp.setTextColor(Color.parseColor(GREEN));
+            imageViewDash.setColorFilter(Color.parseColor(WHITE));
+            imageViewDisp.setColorFilter(Color.parseColor(GREEN));
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         });
-        dialog.show();
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnim;
-        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
     }
 }

a1908c42e8dacff8637c25fdc2f76434423f9732
Tejas Udupa <tejasudupa1285@gmail.com>
2022-07-25 20:14:00 +0530

    tools/Utils: Add new helper methods
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- app/src/main/java/com/example/myapplication3/tools/Utils.java
+++ app/src/main/java/com/example/myapplication3/tools/Utils.java
@@ -1,5 +1,7 @@
 package com.example.myapplication3.tools;
 
+import android.util.Log;
+
 import com.topjohnwu.superuser.Shell;
 
 import java.util.List;
@@ -8,24 +10,56 @@ import java.util.List;
  * added by sharan
  */
 public class Utils {
-    Shell.Result result;
+    final static String TAG = "Utils";
 
-    public List<String> readGetList(String... cmd) throws UtilException{
-        result = Shell.cmd(cmd).exec();
+    public static List<String> readGetList(String... cmd) throws UtilException{
+        Shell.Result result = Shell.cmd(cmd).exec();
         return result.getOut();
     }
 
-    public String read(int index, String... cmd) throws UtilException{
-        result = Shell.cmd(cmd).exec();
-        return result.getOut().get(index);
+    public static String read(int index, String path) throws UtilException{
+        Shell.Result result = Shell.cmd("cat " + path).exec();
+        if(isValaidIO(result.getOut().get(index)))
+            return result.getOut().get(index);
+        else
+            throw new UtilException("Read Error");
     }
 
-    public String[] readGetArr(String... cmd) throws UtilException{
-        result = Shell.cmd(cmd).exec();
-        return (String[])result.getOut().toArray();
+    public static String[] readGetArr(String... cmd) throws UtilException{
+        Shell.Result result = Shell.cmd(cmd).exec();
+        String[] str = result.getOut().toArray(new String[0]);
+        if(isValaidIO(str[0]))
+            return str;
+        else
+            throw new UtilException("Read Error");
     }
 
-    public void write(String... cmd){
+    public static String execCmdRead(int index, String... cmd) throws UtilException{
+        Shell.Result result = Shell.cmd(cmd).exec();
+        if(isValaidIO(result.getOut().get(index)))
+            return result.getOut().get(index);
+        else
+            throw new UtilException("Read Error");
+    }
+
+    public static void write(String value, String path){
+        Shell.cmd("echo " + value + " > " + path).exec();
+    }
+
+    public static void execCmdWrite(String... cmd){
         Shell.cmd(cmd).exec();
+    }
+
+    public static String[] splitStrings(String cmd, String regex){
+        try {
+            String[] arrStr = Utils.readGetArr("cat " + cmd);
+            return arrStr[0].split(regex);
+        }catch (UtilException e){
+            return null;
+        }
+    }
+
+    public static boolean isValaidIO(String out){
+        return !out.contains("cat");
     }
 }

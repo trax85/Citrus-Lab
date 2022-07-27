a524d92a27f31a189ac85d49fb3ea74e2aa4f10e
Tejas Udupa <tejasudupa1285@gmail.com>
2022-07-27 11:30:34 +0530

    ProfileFragment: allow users to add custom scripts
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ app/src/main/java/com/example/myapplication3/fragments/ProfileFragment/CustomProfile.java
@@ -0,0 +1,150 @@
+package com.example.myapplication3.fragments.ProfileFragment;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+import com.example.myapplication3.tools.Utils;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class CustomProfile {
+    final static String Lable1 = "CustProf1";
+    final static String Lable2 = "CustProf2";
+    final static String Lable3 = "CustProf3";
+    final static String Name1 = "ProfName1";
+    final static String Name2 = "ProfName2";
+    final static String Name3 = "ProfName3";
+    final static String TAG = "CustomProfile";
+    SharedPreferences pref;
+    boolean[] isProfPresent;
+    ProfileFragment fragment;
+    boolean[] touched;
+
+    String[] profArr, nameArr, nameTags, lableTags;
+
+    public CustomProfile(ProfileFragment fragment){
+        this.fragment = fragment;
+        //Initialise Arrays
+        nameTags = new String[]{Name1, Name2, Name3};
+        lableTags = new String[]{Lable1, Lable2, Lable3};
+        nameArr = new String[nameTags.length];
+        profArr = new String[lableTags.length];
+        isProfPresent = new boolean[profArr.length];
+        touched = new boolean[]{false, false, false};
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void initCustomProfiles(Context context){
+        pref = context.getSharedPreferences(TAG, Context.MODE_PRIVATE);
+
+        for(int i = 0; i < 3; i++) {
+            profArr[i] = pref.getString(lableTags[i], "null");
+            nameArr[i] = pref.getString(nameTags[i],"Not set");
+        }
+        for(int i = 0; i < profArr.length; i++)
+            isProfPresent[i] = !profArr[i].equals("null");
+        initViews();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void initViews(){
+        for(int i = 0;i < isProfPresent.length; i++){
+            if(isProfPresent[i]){
+                setOnClickListeners(i);
+                hideAddView(i);
+                Log.d(TAG, "profile present");
+            }else{
+                hideView(i);
+                fragment.setAddButton(i);
+                Log.d(TAG, "profile not present");
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void setOnClickListeners(int idx){
+        fragment.unSetTextViews[idx].setOnClickListener(v -> {
+            clearItem(idx);
+            hideView(idx);
+        });
+        fragment.editTextViews[idx].setOnClickListener(v -> {
+            editOpertaion(idx);
+        });
+        fragment.custProfArr[idx].setOnClickListener(v -> {
+            Utils.execCmdWrite(profArr[idx]);
+            Toast.makeText(fragment.getContext(), "Applying profile " + idx,
+                    Toast.LENGTH_LONG).show();
+        });
+        fragment.editNameArr[idx].setOnClickListener(v -> renameOperation(idx));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void renameOperation(int idx){
+        MaterialAlertDialogBuilder builder = new
+                MaterialAlertDialogBuilder(fragment.requireActivity());
+        final EditText weightInput = new EditText(fragment.getActivity());
+
+        builder.setTitle("Edit Name");
+        weightInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        weightInput.setHint(fragment.textViews[idx].getText().toString());
+        builder.setView(weightInput);
+
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            String str = weightInput.getText().toString();
+            fragment.textViews[idx].setText(str);
+            nameArr[idx] = str;
+            setItem(idx);
+        });builder.setNegativeButton("Cancle", (dialog, which) -> dialog.cancel());
+        builder.show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void editOpertaion(int idx){
+        fragment.getFile(idx);
+    }
+
+    public void setItem(int idx){
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(nameTags[idx], nameArr[idx]);
+        editor.putString(lableTags[idx], profArr[idx]);
+        editor.apply();
+    }
+
+    public void clearItem(int idx){
+        SharedPreferences.Editor editor = pref.edit();
+        editor.remove(lableTags[idx]);
+        editor.remove(nameTags[idx]);
+        editor.apply();
+    }
+
+    //called when profile is cleared
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void hideView(int idx){
+        fragment.addProfArr[idx].setVisibility(View.VISIBLE);
+        fragment.editTextViews[idx].setVisibility(View.GONE);
+        fragment.unSetTextViews[idx].setVisibility(View.GONE);
+        fragment.editNameArr[idx].setVisibility(View.INVISIBLE);
+        fragment.textViews[idx].setText("Not set");
+        fragment.setAddButton(idx);
+    }
+
+    //called when profile is added
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void hideAddView(int idx){
+        fragment.addProfArr[idx].setVisibility(View.GONE);
+        fragment.editTextViews[idx].setVisibility(View.VISIBLE);
+        fragment.unSetTextViews[idx].setVisibility(View.VISIBLE);
+        fragment.editNameArr[idx].setVisibility(View.VISIBLE);
+        fragment.textViews[idx].setText(nameArr[idx]);
+        fragment.setAddButton(idx);
+        setOnClickListeners(idx);
+        Log.d(TAG,"name:" + nameArr[idx]);
+    }
+}
--- app/src/main/java/com/example/myapplication3/fragments/ProfileFragment/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication3/fragments/ProfileFragment/ProfileFragment.java
@@ -1,30 +1,56 @@
 package com.example.myapplication3.fragments.ProfileFragment;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RawRes;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication3.R;
 import com.example.myapplication3.tools.Utils;
 import com.google.android.material.button.MaterialButton;
-import com.topjohnwu.superuser.Shell;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.atomic.AtomicReference;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class ProfileFragment extends Fragment {
+    final static String TAG = "ProfileFragment";
+
     MaterialButton power_button, balance_button, perf_button;
+    RelativeLayout prof1, prof2, prof3;
+    RelativeLayout[] custProfArr;
+    ImageView addProf1, addProf2, addProf3, editName1, editName2, editName3;
+    ImageView[] addProfArr, editNameArr;
+    TextView editText1, editText2, editText3, unsetText1, unsetText2, unsetText3;
+    TextView textView1, textView2, textView3;
+    TextView[] textViews, editTextViews, unSetTextViews;
+    CustomProfile cprofile;
+    Intent intent;
+    int curIdx;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +69,8 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initViews(view);
+        cprofile = new CustomProfile(this);
+        cprofile.initCustomProfiles(getContext());
         setListeners();
     }
 
@@ -50,6 +78,34 @@ public class ProfileFragment extends Fragment {
         power_button = view.findViewById(R.id.button_power);
         balance_button = view.findViewById(R.id.button_balance);
         perf_button = view.findViewById(R.id.button_performance);
+        prof1 = view.findViewById(R.id.button_add1);
+        prof2 = view.findViewById(R.id.button_add2);
+        prof3 = view.findViewById(R.id.button_add3);
+        custProfArr = new RelativeLayout[]{prof1, prof2, prof3};
+
+        addProf1 = view.findViewById(R.id.imageViewAdd1);
+        addProf2 = view.findViewById(R.id.imageViewAdd2);
+        addProf3 = view.findViewById(R.id.imageViewAdd3);
+        addProfArr = new ImageView[]{addProf1, addProf2, addProf3};
+
+        textView1 = view.findViewById(R.id.proftext1);
+        textView2 = view.findViewById(R.id.proftext2);
+        textView3 = view.findViewById(R.id.proftext3);
+        textViews = new TextView[]{textView1, textView2, textView3};
+
+        editText1 = view.findViewById(R.id.editbutton1);
+        editText2 = view.findViewById(R.id.editbutton2);
+        editText3 = view.findViewById(R.id.editbutton3);
+        editTextViews = new TextView[]{editText1, editText2, editText3};
+        unsetText1 = view.findViewById(R.id.unsetbutton1);
+        unsetText2 = view.findViewById(R.id.unsetbutton2);
+        unsetText3 = view.findViewById(R.id.unsetbutton3);
+        unSetTextViews = new TextView[]{unsetText1, unsetText2, unsetText3};
+
+        editName1 = view.findViewById(R.id.editName1);
+        editName2 = view.findViewById(R.id.editName2);
+        editName3 = view.findViewById(R.id.editName3);
+        editNameArr = new ImageView[]{editName1, editName2, editName3};
     }
 
     void setListeners(){
@@ -85,5 +141,60 @@ public class ProfileFragment extends Fragment {
             e.printStackTrace();
         }
         return byteArrayOutputStream.toString();
+    }
+
+    public void setAddButton(int idx){
+        custProfArr[idx].setOnClickListener(v -> {
+            getFile(idx);
+            Log.d(TAG, "Set add button");
+        });
+    }
+
+    public void getFile(int idx){
+        intent = new Intent(Intent.ACTION_GET_CONTENT);
+        Uri uri = Uri.parse("/Download"); // a directory
+        intent.setDataAndType(uri, "*/*");
+        curIdx = idx;
+        getFileContentActivity.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> getFileContentActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                Intent intent = result.getData();
+                Uri uri = intent.getData();
+                File file = new File(uri.getPath());
+                String name = file.getName();
+                if(!isValidFile(name)){
+                    Toast.makeText(getContext(),"Invalid file",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                InputStream inputStream;
+                String content;
+                try {
+                    inputStream = getActivity().getContentResolver().openInputStream(uri);
+                    content = new BufferedReader(new InputStreamReader(inputStream))
+                            .lines().collect(Collectors.joining("\n"));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    return;
+                }
+
+                cprofile.profArr[curIdx] = content;
+                cprofile.nameArr[curIdx] = getFileName(file.getName());
+                cprofile.setItem(curIdx);
+                cprofile.hideAddView(curIdx);
+                Log.d(TAG, "idx:" + curIdx + " content:" + content);
+            });
+
+    public boolean isValidFile(String file){
+        return file.contains(".sh");
+    }
+
+    public String getFileName(String name){
+        name = name.split(":")[1];
+        name = name.split("\\.")[0];
+        return name;
     }
 }

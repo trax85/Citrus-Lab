d2d688b262ceb024e7f773c0bce32133368b130a
Tejas Udupa <tejasudupa1285@gmail.com>
2022-09-30 22:52:48 +0530

    ProfileFragment: add new changes to customProfile
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- app/src/main/java/com/example/myapplication3/fragments/ProfileFragment/CustomProfile.java
+++ app/src/main/java/com/example/myapplication3/fragments/ProfileFragment/CustomProfile.java
@@ -1,51 +1,44 @@
 package com.example.myapplication3.fragments.ProfileFragment;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
-
 import com.example.myapplication3.tools.Utils;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class CustomProfile {
-    final static String Lable1 = "CustProf1";
-    final static String Lable2 = "CustProf2";
-    final static String Lable3 = "CustProf3";
-    final static String Name1 = "ProfName1";
-    final static String Name2 = "ProfName2";
-    final static String Name3 = "ProfName3";
+    private final String [] labelTags = new String[]{"CustProf1", "CustProf2", "CustProf3"};
+    private final String[] nameTags = new String[]{"ProfName1", "ProfName2", "ProfName3"};
     final static String TAG = "CustomProfile";
-    SharedPreferences pref;
-    boolean[] isProfPresent;
-    ProfileFragment fragment;
+    private SharedPreferences pref;
+    public static boolean[] isProfPresent;
+    private final ProfileFragment fragment;
     boolean[] touched;
 
-    String[] profArr, nameArr, nameTags, lableTags;
+    String[] profArr, nameArr;
 
     public CustomProfile(ProfileFragment fragment){
         this.fragment = fragment;
         //Initialise Arrays
-        nameTags = new String[]{Name1, Name2, Name3};
-        lableTags = new String[]{Lable1, Lable2, Lable3};
+        profArr = new String[labelTags.length];
         nameArr = new String[nameTags.length];
-        profArr = new String[lableTags.length];
         isProfPresent = new boolean[profArr.length];
         touched = new boolean[]{false, false, false};
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void initCustomProfiles(Context context){
         pref = context.getSharedPreferences(TAG, Context.MODE_PRIVATE);
 
         for(int i = 0; i < 3; i++) {
-            profArr[i] = pref.getString(lableTags[i], "null");
+            profArr[i] = pref.getString(labelTags[i], "null");
             nameArr[i] = pref.getString(nameTags[i],"Not set");
         }
         for(int i = 0; i < profArr.length; i++)
@@ -53,28 +46,24 @@ public class CustomProfile {
         initViews();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void initViews(){
+        Handler handler = new Handler(Looper.getMainLooper());
         for(int i = 0;i < isProfPresent.length; i++){
             if(isProfPresent[i]){
+                Log.d(TAG,"profile found");
                 setOnClickListeners(i);
                 hideAddView(i);
             }else{
+                Log.d(TAG,"no profile");
                 hideView(i);
-                fragment.setAddButton(i);
+                int finalI = i;
+                handler.post(() -> fragment.setAddButton(finalI));
             }
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void setOnClickListeners(int idx){
-        fragment.unSetTextViews[idx].setOnClickListener(v -> {
-            clearItem(idx);
-            hideView(idx);
-        });
-        fragment.editTextViews[idx].setOnClickListener(v -> {
-            editOpertaion(idx);
-        });
+        fragment.editTextViews[idx].setOnClickListener(v -> fragment.showPopUpWindow(idx));
         fragment.custProfArr[idx].setOnClickListener(v -> {
             Utils.execCmdString(profArr[idx]);
             Toast.makeText(fragment.getContext(), "Applying profile " + (1 + idx),
@@ -83,7 +72,6 @@ public class CustomProfile {
         fragment.editNameArr[idx].setOnClickListener(v -> renameOperation(idx));
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void renameOperation(int idx){
         MaterialAlertDialogBuilder builder = new
                 MaterialAlertDialogBuilder(fragment.requireActivity());
@@ -99,49 +87,44 @@ public class CustomProfile {
             fragment.textViews[idx].setText(str);
             nameArr[idx] = str;
             setItem(idx);
-        });builder.setNegativeButton("Cancle", (dialog, which) -> dialog.cancel());
+        });builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
         builder.show();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public void editOpertaion(int idx){
-        fragment.getFile(idx);
-    }
-
     public void setItem(int idx){
+        Log.d(TAG, "setItem");
         SharedPreferences.Editor editor = pref.edit();
         editor.putString(nameTags[idx], nameArr[idx]);
-        editor.putString(lableTags[idx], profArr[idx]);
+        editor.putString(labelTags[idx], profArr[idx]);
         editor.apply();
     }
 
     public void clearItem(int idx){
         SharedPreferences.Editor editor = pref.edit();
-        editor.remove(lableTags[idx]);
+        editor.remove(labelTags[idx]);
         editor.remove(nameTags[idx]);
         editor.apply();
     }
 
     //called when profile is cleared
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("SetTextI18n")
     public void hideView(int idx){
         fragment.addProfArr[idx].setVisibility(View.VISIBLE);
         fragment.editTextViews[idx].setVisibility(View.GONE);
-        fragment.unSetTextViews[idx].setVisibility(View.GONE);
         fragment.editNameArr[idx].setVisibility(View.INVISIBLE);
         fragment.textViews[idx].setText("Not set");
         fragment.setAddButton(idx);
+        isProfPresent[idx] = false;
     }
 
     //called when profile is added
-    @RequiresApi(api = Build.VERSION_CODES.N)
     public void hideAddView(int idx){
         fragment.addProfArr[idx].setVisibility(View.GONE);
         fragment.editTextViews[idx].setVisibility(View.VISIBLE);
-        fragment.unSetTextViews[idx].setVisibility(View.VISIBLE);
         fragment.editNameArr[idx].setVisibility(View.VISIBLE);
         fragment.textViews[idx].setText(nameArr[idx]);
         fragment.setAddButton(idx);
+        isProfPresent[idx] = true;
         setOnClickListeners(idx);
     }
 }
